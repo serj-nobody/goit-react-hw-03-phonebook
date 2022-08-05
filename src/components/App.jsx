3c6b03bf0786552abddcfa2719fe794c051c8170
Serj <mrnobody959@gmail.com>
2022-08-05 16:28:48 +0300

    complete phonebook hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,6 +48,20 @@ export class App extends Component {
     return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
   }
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const filterResults = this.getFilterResults();
